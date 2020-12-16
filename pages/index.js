fcc7f3be6301eb952e65474245cc6cb7f24a95b6
Eliezer <eliezereliwork@gmail.com>
2020-12-16 12:31:07 +0200

    work law

---
+++ pages/index.js
@@ -0,0 +1,222 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>עורך דין דיני עבודה</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+        צור קשר <a href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/">דיני עבודה</a>
+        </h1>
+
+        <p className="description">
+          אם נפצעת <code>תתבע</code>
+        </p>
+
+        <div className="grid">
+          <a href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/" className="card">
+            <h3>דיני עבודה &rarr;</h3>
+            <p>אם זה קרה לך , תתבע. אל תוותר, אל תכעס, תתבע. צור קשר עם עורך דין </p>
+          </a>
+
+          <a href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/" className="card">
+            <h3>צור קשר &rarr;</h3>
+            <p>עורך דין דיני עבודה</p>
+          </a>
+
+          <a
+            href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/"
+            className="card"
+          >
+            <h3>דיני עבודה &rarr;</h3>
+            <p>עורך דין לדיני עבודה, עוד דיני עבודה, עורך דין דיני עבודה</p>
+          </a>
+
+          <a
+            href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/"
+            className="card"
+          >
+            <h3>דיני עבודה &rarr;</h3>
+            <p>
+              אל תצא פראייר תתטבע את המעסיק שלך, יש חוקים פה.
+            </p>
+          </a>
+          <a
+            href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/"
+            className="card"
+          ></a>
+        </div>
+      </main>
+      <div>
+        <h1 className="title">
+         עורך דין{' '}
+          <Link href="/posts/first-post">
+          <a>בעמוד זה</a>
+          </Link>
+        </h1>
+      </div>
+
+      <footer>
+        <a
+          href="https://segelstein.com/%d7%93%d7%99%d7%a0%d7%99-%d7%a2%d7%91%d7%95%d7%93%d7%94/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,49 @@
+export default function FirstPost() {
+    return (<div  className="container">
+
+          <h1>עורך דין</h1>
+            <h2>
+              <Link href="/">
+                <a>Back to home</a>
+              </Link>
+            </h2>
+
+            <h1>עורך דין תותח </h1>
+            <a href="http://www.2all.co.il/web/Sites19/zegelshten/DEFAULT.asp">תאונת עבודה</a>
+            <h1>עורך דין מלך</h1>
+            <a href="https://xn----6hceqci6ar9c.yolasite.com/">תאונת עבודה</a>
+            <a href= "https://segelstein.com/">דיני עבודה</a>
+            <a href="http://www.hup.co.il/link/%D7%A2%D7%95%D7%A8%D7%9A-%D7%93%D7%99%D7%9F/63118-%D7%96%D7%92%D7%9C%D7%A9%D7%98%D7%99%D7%99%D7%9F-%D7%9E%D7%A9%D7%A8%D7%93-%D7%A2%D7%95%D7%93">תאונת עבודה</a>
+            <a href= "https://workaccident.vercel.app/">תאונת עבודה</a>
+
+            <style jsx>{`
+            .container {
+              min-height: 100vh;
+              padding: 0 0.5rem;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+            a {
+              color: #0070f3;
+              text-decoration: none;
+            }
+            `}</style>
+  <style jsx global>{`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+      sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+`}</style>
+       </div>
+    )}
